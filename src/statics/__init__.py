f4fe53e86ea5664a7591d146584d4b2632009447
Matthew Davidson <admin@davidson.engineering>
2024-11-12 23:07:23 -0800

    added linear solver to exposed imports

--- src/statics/__init__.py
+++ src/statics/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.1.0"
 
-from statics.statics import BoundVector, Reaction, Load, ReactionSolver
+from statics.statics import BoundVector, Reaction, Load, ReactionSolver, LinearSolver
 from statics.statics import (
     UnderconstrainedError,
     IllConditionedError,
